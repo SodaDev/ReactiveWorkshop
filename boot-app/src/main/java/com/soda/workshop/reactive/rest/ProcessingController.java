6212df9b0816ea47aabbc3b13acff26d347a1ec0
Marcin Sodkiewicz <sodkiewicz.m@gmail.com>
2017-03-04 15:31:10 +0100

    Rx boot endpoint

--- boot-app/src/main/java/com/soda/workshop/reactive/rest/ProcessingController.java
+++ boot-app/src/main/java/com/soda/workshop/reactive/rest/ProcessingController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
+import rx.Observable;
 
 import java.util.Timer;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class ProcessingController {
@@ -34,6 +36,23 @@ public class ProcessingController {
         DeferredTask task = new DeferredTask(processingTimeMs, deferredResult);
 
         timer.schedule(task, processingTimeMs);
+
+        return deferredResult;
+    }
+
+    /**
+     * curl "http://localhost:9090/non-blocking?delay=1500"
+     */
+    @RequestMapping("/non-blocking-rx")
+    public DeferredResult<Result> nonBlockingProcessingRx(@RequestParam(value = "delay") int processingTimeMs) {
+        DeferredResult<Result> deferredResult = new DeferredResult<>();
+        Observable.just(Result.builder()
+                .processed(true)
+                .processingTimeMs(processingTimeMs)
+                .build()
+        )
+                .delay(processingTimeMs, TimeUnit.MILLISECONDS)
+                .forEach(deferredResult::setResult, deferredResult::setErrorResult);
 
         return deferredResult;
     }
